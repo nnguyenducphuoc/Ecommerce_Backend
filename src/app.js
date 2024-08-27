266e8b0f4c5cbbfb01c3b3d152b92a906c3d971f
Nguyen Duc Phuoc <nguyenducphuoc0802@gmail.com>
2024-08-28 00:37:29 +0700

    connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const { checkOverLoad } = require("./helper/check.connect.js");
+
 const app = express();
 
 // init middleware
-
+// morgan -> in ra cac log
+app.use(morgan("dev"));
+// helmet -> che dau ca thong tin header
+app.use(helmet());
+// compression -> giam bang thong
+app.use(compression());
 // init db
-
+require("./databases/init.mongodb.js");
+checkOverLoad();
 // init routes
 
 // handling error
