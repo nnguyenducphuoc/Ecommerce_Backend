10230104e65101a04269890857d9e974a7696be3
Nguyen Duc Phuoc <nguyenducphuoc0802@gmail.com>
2024-09-18 02:18:44 +0700

    add permissions and apikey

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,46 @@
+const { findById } = require("../services/apikey.service");
+
+const HEADER = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    if (!key) {
+      return res.status(403).json({
+        message: "Forbidden Error",
+      });
+    }
+    // check objKey
+    const objKey = await findById(key);
+    if (!objKey) {
+      return res.status(403).json({
+        message: "Forbidden Error",
+      });
+    }
+    req.objKey = objKey;
+    return next();
+  } catch (err) {}
+};
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    if (!req.objKey.permissions) {
+      return res.status(403).json({
+        message: "Permission Denied",
+      });
+    }
+    console.log("permissions::", req.objKey.permissions);
+    const validPermissions = req.objKey.permissions.includes(permission);
+    if (!validPermissions) {
+      return res.status(403).json({
+        message: "Permission Denied",
+      });
+    }
+    return next();
+  };
+};
+
+module.exports = { apiKey, permission };
